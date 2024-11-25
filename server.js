27a53b190251c333c2c03cc472feb3b7ca875d48
Lipenncom1 <148969818+Lipenncom1@users.noreply.github.com>
2024-11-26 04:27:48 +0800

    update the rest of view part. update function still need to fix

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//const cors = require('cors');
+//app.use(cors());
+//app.get('/favicon.ico', (req, res) => res.status(204));
 
 //login
 const users_login = {
@@ -108,48 +111,110 @@ const handle_FindAll = async (res) => {
 };
 
 const findOneAnimalDocument = async (db, criteria) => {
-    const collection = db.collection(collectionName).sort({relevanceField: -1});//根据相关性对结果降序排列
+    const collection = db.collection(collectionName);
     return await collection.find(criteria).toArray();
+    console.log("findOnaAnimalDocument", collection)
 };
 
 const handle_FindOne = async (req, res) => {
+    let db;
+    try {
+        await client.connect();
+        console.log("Connected successfully to server");
+        db = client.db(dbName);
+
+        console.log("Request body:", req.body);
+
+        // create query
+        const criteria = {
+            Location: new RegExp(req.body.Location, 'i'), // 'i' 表示忽略大小写
+            Type: new RegExp(req.body.Type, 'i'),
+            Breed: new RegExp(req.body.Breed, 'i'),
+            Gender: new RegExp(req.body.Gender, 'i'),
+            Prominent_Features: new RegExp(req.body.Prominent_Features, 'i'),
+            Disabilities: new RegExp(req.body.Disabilities, 'i'),
+            Adopted: new RegExp(req.body.Adopted,'i')
+        };
+        
+        console.log("Search criteria:", criteria);//for debug
+        // 移除空或未定义的键
+        Object.keys(criteria).forEach(key => {
+            if (!criteria[key]) {
+                delete criteria[key];
+            }
+        });
+        console.log("Search criteria2:", criteria);//for debug
+
+        const foundAnimals = await findOneAnimalDocument(db, criteria);
+        console.log("Found Results :", foundAnimals);
+
+        if (foundAnimals.length > 0) {
+            res.status(200).render('view1', { variable1: foundAnimals});
+        } else {
+            console.log('No results found');
+            res.redirect('/history');
+        }
+    } catch (error) {
+        console.error("Error occurred:", error);
+        res.status(500).send("Internal server error");
+    } finally { 
+    	if (db) {
+    		await client.close();
+    	}
+    }
+};
+
+const passAnimalDocument = async (req, res) => {
     await client.connect();
     console.log("Connected successfully to server");
     const db = client.db(dbName);
-    const criteria = {
-        a: req.body.Location,
-        b: req.body.Type,
-        c: req.body.Breed,
-        d: req.body.Gender,
-        e: req.body.Prominent_Features,
-        f: req.body.Disabilities,
-        g: req.body.Adopted,
-    };
-    // remove empty key or undefined key
-    Object.keys(criteria).forEach(key => {
-        if (!criteria[key]) {
-            delete criteria[key];
-        }
-    });
-    
-    const animals = await findOneAnimalDocument(db,criteria);
-    console.log("get result", animals);
-    if (animals.length > 0) {
-        res.status(200).render('view1', { variable1: animals })
-    } else {
-        console.log('Cant find')
-        res.redirect('/history')
-    }
-    /*
-    let DOCID = {_id: ObjectId.createFromHexString(req.body._id)};
+    const DOCID = {_id: new ObjectId(req.params.id)};
     const animal = await findOneAnimalDocument(db, DOCID);
-    if (animal.length > 0 && animal[0].userid === req.user.id){
-        res.status(200).render('view1', {animal: animalOne})
+
+    if(animal.length > 0) {
+        res.render('view_update', {animal: animal[0]});
+    } else {
+        console.log('find method wrong or Animal not found')
     }
-    //const animalOne = [...animal];
-    await client.close();
-    console.log("Closed DB connection")*/
-    
+}
+
+//update View 
+const updateDocument = async (db, criteria, updateData) => {
+	var collection = db.collection(collectionName);
+	let results = await collection.updateOne(criteria,{$set: updateData});
+	return results;
+}
+
+
+///errrrrrrrrrooooooooooorrrrrrrrr 
+const handle_UpdateSaveView = async (req, res) => {
+	await client.connect();
+	console.log("Connected successfully to server");
+	const db = client.db(dbName);
+	//let DOCID = {_id: ObjectId(req.params.id)};
+	let DOCID = {_id: new ObjectId(req.params.id)};
+	const docs = await findOneAnimalDocument(db, DOCID);
+	console.log("Documents found:", docs);
+	if (docs.length > 0 && docs[0]._id === req._id) {
+		const updateData = {
+			Animal_name: req.body.Animal_name,
+			Type: req.body.Type,
+			Breed: req.body.Breed,
+			Gender: req.body.Gender,
+			Location: req.body.Location,
+			Prominent_Features: req.body.Prominent_Features,
+			Disabilities: req.body.Disabilities,
+			Adopted: req.body.Adopted
+		};
+		console.log("Update data:", updateData);
+		const results = await updateDocument(db, DOCID, updateData);
+		console.log("Update results:", results);
+		res.status(200).render('info', {message: `Updated ${results.modifiedCount} document(s)`, user: req.user});
+		 //res.render('history');
+		 res.redirect('history');
+	} else {
+		res.status(500).render('info', {message: 'save error !', user: req.user});	
+	}
 }
 
 //DELETE
@@ -220,8 +285,21 @@ app.get("/view", isLoggedIn, (req, res) => {
 
 //view_find POST
 app.post('/view', isLoggedIn, async (req, res) => {
-    console.log("view find req")
+    console.log("view find req");
     await handle_FindOne(req, res);
+})
+
+//view_update get
+// use /:id 从view1.ejs update button处拿 animal._id
+app.get('/update/:id', isLoggedIn, async(req, res) => {
+    console.log("update start!");
+    await passAnimalDocument(req, res);
+})
+
+//view_save update
+app.post('/update/:id', isLoggedIn, async(req, res) => {
+	console.log("update to save");
+	await handle_UpdateSaveView(req, res, req.query);	
 })
 
 app.get("/report", isLoggedIn, (req, res) => {
@@ -240,8 +318,10 @@ app.get("/information", isLoggedIn, (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-    res.status(404).render('information', { message: `${req.path} - Unknown request!` });
+    res.status(404).render('information', { message: `${req.path} - Unknown request! `});
 });
 
 const port = process.env.PORT || 8099;
-app.listen(port, () => { console.log(`Listening at http://localhost:${port}`); });
+app.listen(port, () => { 
+    console.log(`Listening at http://localhost:${port}`); 
+});
